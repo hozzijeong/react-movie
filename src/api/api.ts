56fe739f5a9deaf0a4b6be6dcc8071107e01f67c
hozzijeong <ghwls0905@naver.com>
2022-06-19 22:25:06 +0900

    Feat: imagePath  및 home 화면 구성

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,33 @@
 const API_KEY = "83abefa42986ae190c0bbb24c6d2e0ae";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface GetMovieList {
+  dates: { maximum: string; minimum: string };
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
-    (response) => response.json()
+    (response) => response.json(),
   );
 }
