4b22ac6baa2df70f15834ea820813d7bf18db21c
hozzijeong <ghwls0905@naver.com>
2022-06-27 16:13:00 +0900

    Feat: Search 검색 결과 없을 경우 설정

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -7,7 +7,7 @@ import { GetMovieList, GetTvList, searchMovie, searchTVShow } from "../api/api";
 import { curContentData } from "../atom";
 import Box from "../components/Box";
 import { Overlay } from "../components/Overlay";
-import { CategoryTitle, Loader, Wrapper } from "../components/Styled";
+import { BigTitle, CategoryTitle, Loader, Wrapper } from "../components/Styled";
 
 const GridTemplate = styled(motion.div)`
   display: grid;
@@ -22,11 +22,24 @@ function Search() {
   const { data: movies, isLoading: movieIsLoading } = useQuery<GetMovieList>(
     ["searchMovie", keyword],
     () => searchMovie(keyword),
+    {
+      onError: (e) => {
+        console.log(e);
+      },
+    },
   );
   const { data: tvShow, isLoading: tvShowIsLoading } = useQuery<GetTvList>(
     ["searchTv", keyword],
     () => searchTVShow(keyword),
+    {
+      onError: (e) => {
+        console.log(e);
+      },
+    },
   );
+
+  const isConsits =
+    movies?.results.length === 0 && tvShow?.results.length === 0;
 
   const [curContent, setCurContent] = useRecoilState(curContentData);
 
@@ -34,12 +47,16 @@ function Search() {
 
   return (
     <Wrapper>
+      <div style={{ height: "30vh" }}></div>
       {isLoading ? (
         <Loader>Loading...</Loader>
+      ) : isConsits ? (
+        <BigTitle style={{ textAlign: "center" }}>There Is No Result</BigTitle>
       ) : (
         <>
-          <div style={{ height: "30vh" }}></div>
-          <CategoryTitle>Movies</CategoryTitle>
+          <CategoryTitle>
+            {movies?.results.length !== 0 ? "Movies" : null}
+          </CategoryTitle>
           <GridTemplate onHoverStart={() => setCurContent(movies?.results)}>
             {movies?.results
               .filter((x) => x.backdrop_path !== "")
@@ -48,7 +65,9 @@ function Search() {
               ))}
           </GridTemplate>
 
-          <CategoryTitle>Tv Show</CategoryTitle>
+          <CategoryTitle>
+            {tvShow?.results.length !== 0 ? "Tv Show" : null}
+          </CategoryTitle>
           <GridTemplate onHoverStart={() => setCurContent(tvShow?.results)}>
             {tvShow?.results
               .filter((x) => x.backdrop_path !== "")
